acb9147e574628dec7723897247c8b5c4771b382
alex0112 <alarsen0112@gmail.com>
2022-10-15 20:01:49 -0600

    get url to gif page

--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,31 @@ use reqwest::blocking::{get};
 use url::{Url};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // let html =
-    //     get("https://getyarn.io/yarn-clip/84e0913e-9df9-4e44-90dd-e25a079bae86")?
-    //     .text()?;
-
     let given_url = Url::parse("https://getyarn.io/yarn-clip/84e0913e-9df9-4e44-90dd-e25a079bae86").unwrap();
 
-    println!("{:#?}", given_url.as_str());
-    println!("{:#?}", given_url.domain().unwrap());
+    let gif_url = fetch_gif(given_url);
+    println!("{:#?}", gif_url.unwrap().as_str());
 
     Ok(())
 }
 
 fn fetch_gif(url: Url) -> Result<String, String> {
     let html =
-        get(url.as_str().to_string())?
-        .text()?;
+        get(url.as_str().to_string()).unwrap()
+        .text();
 
-    //let gif_href = scrape_gif_url(&html).unwrap();
+    let gif_href = scrape_gif_url(&html.unwrap().to_string()).unwrap();
+    let gif_url = url.join(&gif_href).unwrap();
 
-    Ok(html)
+
+    Ok(gif_url.to_string())
 }
 
-// fn scrape_gif_url(original_html: &str) -> Result<&str, Box<dyn std::error::Error>> {
-//     let document = Html::parse_document(original_html);
-//     let gif_link_selector = Selector::parse("td.bord:nth-child(3) > a:nth-child(1)").unwrap();
-//     let anchor = document.select(&gif_link_selector).next().unwrap();
-//     let href = anchor.value().attr("href").unwrap();
+fn scrape_gif_url(original_html: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let document = Html::parse_document(original_html);
+    let gif_link_selector = Selector::parse("td.bord:nth-child(3) > a:nth-child(1)").unwrap();
+    let anchor = document.select(&gif_link_selector).next().unwrap();
+    let href = anchor.value().attr("href").unwrap().to_string();
 
-//     Ok(href)
-// }
-
+    Ok(href)
+}
