17735802d99db35d2c4c97bcff8c0afc8134bff2
alex0112 <alarsen0112@gmail.com>
2023-07-27 14:29:56 -0600

    parse url as a command line arg

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,23 @@ use reqwest::blocking::{get};
 use url::{Url, ParseError};
 use regex::Regex;
 use std::io::{self, Write};
+use std::env;
 
 /////////////////////////////////////////////////////////////////////
 // https://y.yarn.co/84e0913e-9df9-4e44-90dd-e25a079bae86_text.gif //
 /////////////////////////////////////////////////////////////////////
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let given_url = Url::parse("https://getyarn.io/yarn-clip/84e0913e-9df9-4e44-90dd-e25a079bae86").unwrap();
+    //    let given_url = Url::parse("https://getyarn.io/yarn-clip/84e0913e-9df9-4e44-90dd-e25a079bae86").unwrap();
+
+
+    let args: Vec<String> = env::args().collect();
+    let given_url = Url::parse(&args[1]).expect("Either the URL provided was invalid or not provided");
 
     assert!(is_yarn_url(&given_url));
     let uid = capture_uid(&given_url).unwrap();
     let gif_url = raw_gif_url(uid).unwrap().to_string();
     let raw = get(gif_url).unwrap().bytes().unwrap();
-
 
     io::stdout().write_all(&raw)?;
 
